150b15682d26a4b1d817f74ee3060b2e99c3ba99
Katende Ronnie <katenderonnie045@gmail.com>
2025-09-19 15:58:21 +0300

    cors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    exit(0);
+}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "katenderonnie045.com"; // Change this to your email
     $subject = "NewRadio5GConsulting Contact form " . $_POST["name"];
